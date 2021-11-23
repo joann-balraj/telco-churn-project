da9ca2fb335de135da2447ee4a3e0c1b2899cbfe
joann <joann.balraj@gmail.com>
2021-11-23 09:03:19 -0600

    adding modules

--- explore.py
+++ explore.py
@@ -1,60 +1,220 @@
-import numpy as np
 import pandas as pd
-import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from scipy import stats
 from env import host, user, password
 
-def prep_data(df):
+def train_validate_test_split(df, target, seed=123):
     '''
-    This function takes in the telco data frame and prepares it for analysis by:
-    * Setting index to customer_id
-    * Deals with the strings indicating "No internet / phone service" by replacing those values with 0
-    * Converts yes and no values to boolean int64 datatypes (0s and 1s) 
-    * Bins the 4 payment methods into two groups: Auto Pay Methods and Manual Pay Methods
-    * Encodes the gender feature and returns a gender_male column with booleans
-    * Drops dupicate columns for in favor of keeping the corresponding columns where the features are already encoded
-    * Deals with empty strings in the total_charges column. These values are empty because the corresponding 
-    customer's tenure is one month or less and total_charges have not accumulated yet. These values are filled with their corresponding monthly_charges.
-    total_charges column is then coverted to float64 datatype. 
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
     '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
+
+
+def explore_univariate(train, cat_vars, quant_vars):
+    for var in cat_vars:
+        explore_univariate_categorical(train, var)
+        print('_________________________________________________________________')
+    for col in quant_vars:
+        p, descriptive_stats = explore_univariate_quant(train, col)
+        plt.show(p)
+        print(descriptive_stats)
+        
+def explore_bivariate(train, target, cat_vars, quant_vars):
+    for cat in cat_vars:
+        explore_bivariate_categorical(train, 'churn', cat)
+    for quant in quant_vars:
+        explore_bivariate_quant(train, 'churn', quant)
+
+def explore_multivariate(train, target, cat_vars, quant_vars):
+    '''
+    '''
+    plot_swarm_grid_with_color(train, target, cat_vars, quant_vars)
+    plt.show()
+    violin = plot_violin_grid_with_color(train, target, cat_vars, quant_vars)
+    plt.show()
+    pair = sns.pairplot(data=train, vars=quant_vars, hue='churn')
+    plt.show()
+    plot_all_continuous_vars(train, 'churn', quant_vars)
+    plt.show()    
+
+
+### Univariate
+
+def explore_univariate_categorical(train, cat_var):
+    '''
+    takes in a dataframe and a categorical variable and returns
+    a frequency table and barplot of the frequencies. 
+    '''
+    frequency_table = freq_table(train, cat_var)
+    plt.figure(figsize=(2,2))
+    sns.barplot(x=cat_var, y='Count', data=frequency_table, color='white', edgecolor='grey')
+    plt.title(cat_var)
+    plt.show()
+    print(frequency_table)
+
+def explore_univariate_quant(train, quant_var):
+    '''
+    takes in a dataframe and a quantitative variable and returns
+    descriptive stats table, histogram, and boxplot of the distributions. 
+    '''
+    descriptive_stats = train[quant_var].describe()
+    plt.figure(figsize=(8,2))
+
+    p = plt.subplot(1, 2, 1)
+    p = plt.hist(train[quant_var], color='white', edgecolor='grey')
+    p = plt.title(quant_var)
+
+    # second plot: box plot
+    p = plt.subplot(1, 2, 2)
+    p = plt.boxplot(train[quant_var])
+    p = plt.title(quant_var)
+    return p, descriptive_stats
     
-    #Set Customer ID as the index
-    df.set_index('customer_id', drop=True, inplace=True)
+def freq_table(train, cat_var):
+    '''
+    for a given categorical variable, compute the frequency count and percent split
+    and return a dataframe of those values along with the different classes. 
+    '''
+    class_labels = list(train[cat_var].unique())
+
+    frequency_table = (
+        pd.DataFrame({cat_var: class_labels,
+                      'Count': train[cat_var].value_counts(normalize=False), 
+                      'Percent': round(train[cat_var].value_counts(normalize=True)*100,2)}
+                    )
+    )
+    return frequency_table
+
+
+#### Bivariate
+
+def explore_bivariate_categorical(train, target, cat_var):
+    '''
+    takes in categorical variable and binary target variable, 
+    returns a crosstab of frequencies
+    runs a chi-square test for the proportions
+    and creates a barplot, adding a horizontal line of the overall rate of the target. 
+    '''
+    print(cat_var, "\n_____________________\n")
+    ct = pd.crosstab(train[cat_var], train[target], margins=True)
+    chi2_summary, observed, expected = run_chi2(train, cat_var, target)
+    p = plot_cat_by_target(train, target, cat_var)
     
-    #Convert strings in features that do not match "yes" or "no"
-    df.replace({'No internet service': "No" , 'No phone service': "No"}, inplace=True)
+    print(chi2_summary)
+    print("\nobserved:\n", ct)
+    print("\nexpected:\n", expected)
+    plt.show(p)
+    print("\n_____________________\n")
 
-    #Convert features with yes or no to 0s and 1s
-    df.replace({'Yes': 1, 'No': 0}, inplace=True)
+def explore_bivariate_quant(train, target, quant_var):
+    '''
+    descriptive stats by each target class. 
+    compare means across 2 target groups 
+    boxenplot of target x quant
+    swarmplot of target x quant
+    '''
+    print(quant_var, "\n____________________\n")
+    descriptive_stats = train.groupby(target)[quant_var].describe()
+    average = train[quant_var].mean()
+    mann_whitney = compare_means(train, target, quant_var)
+    plt.figure(figsize=(4,4))
+    boxen = plot_boxen(train, target, quant_var)
+    swarm = plot_swarm(train, target, quant_var)
+    plt.show()
+    print(descriptive_stats, "\n")
+    print("\nMann-Whitney Test:\n", mann_whitney)
+    print("\n____________________\n")
 
-    #Group payment_type_id in two subgroups(auto pay vs manual pay) with booleans (True = auto_pay)
-    df.payment_type_id.replace({1: 0, 2: 0, 3: 1, 4: 1}, inplace=True)
+## Bivariate Categorical
 
-    #Rename payment_type_id columns to auto_bill_pay 
-    df.rename(columns={'payment_type_id': 'auto_bill_pay'}, inplace=True)
+def run_chi2(train, cat_var, target):
+    observed = pd.crosstab(train[cat_var], train[target])
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    chi2_summary = pd.DataFrame({'chi2': [chi2], 'p-value': [p], 
+                                 'degrees of freedom': [degf]})
+    expected = pd.DataFrame(expected)
+    return chi2_summary, observed, expected
 
+def plot_cat_by_target(train, target, cat_var):
+    p = plt.figure(figsize=(2,2))
+    p = sns.barplot(cat_var, target, data=train, alpha=.8, color='lightseagreen')
+    overall_rate = train[target].mean()
+    p = plt.axhline(overall_rate, ls='--', color='gray')
+    return p
     
-    #Columns to encode
-    cols_to_encode = ['gender']
 
-    #Encode
-    dummies = pd.get_dummies(df[cols_to_encode], drop_first=[True])
+## Bivariate Quant
 
-    #Concat dummies to original df
-    df = pd.concat([df, dummies], axis=1)
+def plot_swarm(train, target, quant_var):
+    average = train[quant_var].mean()
+    p = sns.swarmplot(data=train, x=target, y=quant_var, color='lightgray')
+    p = plt.title(quant_var)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
 
-    #Columns to drop
-    cols_to_drop = ['gender','internet_service_type', 'contract_type', 'payment_type']
+def plot_boxen(train, target, quant_var):
+    average = train[quant_var].mean()
+    p = sns.boxenplot(data=train, x=target, y=quant_var, color='lightseagreen')
+    p = plt.title(quant_var)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
 
-    #drop columns
-    df.drop(columns=cols_to_drop, inplace=True)
+# alt_hyp = ‘two-sided’, ‘less’, ‘greater’
 
-    #Fill empty values in total_charges column where tenure is less than 1 month
-    df.total_charges = df.total_charges.replace(' ', 0.0)
+def compare_means(train, target, quant_var, alt_hyp='two-sided'):
+    x = train[train[target]==0][quant_var]
+    y = train[train[target]==1][quant_var]
+    return stats.mannwhitneyu(x, y, use_continuity=True, alternative=alt_hyp)
 
-    #Convert total_charges to float
-    df.total_charges = df.total_charges.astype('float64')
 
-    #Replace 0s in total charges with monthly charge
-    df.total_charges = np.where(df.total_charges==0.0, df.monthly_charges , df.total_charges)
+### Multivariate
 
-    return df
+def plot_all_continuous_vars(train, target, quant_vars):
+    '''
+    Melt the dataset to "long-form" representation
+    boxenplot of measurement x value with color representing survived. 
+    '''
+    my_vars = [item for sublist in [quant_vars, [target]] for item in sublist]
+    sns.set(style="whitegrid", palette="twilight_shifted")
+    melt = train[my_vars].melt(id_vars=target, var_name="measurement")
+    plt.figure(figsize=(8,6))
+    p = sns.boxenplot(x="measurement", y="value", hue=target, data=melt)
+    p.set(yscale="log", xlabel='')    
+    plt.show()
+
+def plot_violin_grid_with_color(train, target, cat_vars, quant_vars):
+    cols = len(cat_vars)
+    for quant in quant_vars:
+        _, ax = plt.subplots(nrows=1, ncols=cols, figsize=(16, 4), sharey=True)
+        for i, cat in enumerate(cat_vars):
+            sns.violinplot(x=cat, y=quant, data=train, split=True, 
+                           ax=ax[i], hue=target, palette="twilight_shifted")
+            ax[i].set_xlabel('')
+            ax[i].set_ylabel(quant)
+            ax[i].set_title(cat)
+        plt.show()
+    
+def plot_swarm_grid_with_color(train, target, cat_vars, quant_vars):
+    cols = len(cat_vars)
+    for quant in quant_vars:
+        _, ax = plt.subplots(nrows=1, ncols=cols, figsize=(16, 4), sharey=True)
+        for i, cat in enumerate(cat_vars):
+            sns.swarmplot(x=cat, y=quant, data=train, ax=ax[i], hue=target, palette="twilight_shifted")
+            ax[i].set_xlabel('')
+            ax[i].set_ylabel(quant)
+            ax[i].set_title(cat)
+        plt.show()
